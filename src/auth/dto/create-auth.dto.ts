73130bc40f2bb3848de7b9180d72de00ebab2293
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2025-01-22 20:31:07 +0530

    initial commit

---
+++ src/auth/dto/create-auth.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword, MinLength } from "class-validator";
+
+export class CreateAuthDto {
+    @IsNotEmpty()
+    username:string;
+    
+    @IsNotEmpty()
+    @IsEmail()
+    email:string;
+    @IsNotEmpty()
+    @MinLength(8)
+    @IsStrongPassword()
+    password:string;
+}
