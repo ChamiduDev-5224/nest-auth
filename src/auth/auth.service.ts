98bf77aafc137d300ce02981b6b5d83e1104166e
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2025-01-24 00:24:13 +0530

    auth signup added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    console.log(createAuthDto);
-    
-    return 'This action adds a new auth';
-  }
+  constructor(private prisma:PrismaService){}
+
+ async create(createAuthDto: CreateAuthDto) {
+
+  try {
+    //check email unique or not already 
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: createAuthDto.email },
+    });
+
+    // If adding email exist 
+    if (existingUser) {
+    return 'Email is already in use. Please use a different email.';
+    }
+
+    //password hashing 
+    const hashPassword = await bcrypt.hash(createAuthDto.password, 4);
+
+    //add data 
+    return this.prisma.user.create({
+    data:{
+      email:createAuthDto.email,
+      username:createAuthDto.username,
+      hashedPassword:hashPassword,
+      status:createAuthDto.status
+    }
+    });   
+      
+  } catch (error) {
+    return error;
+  } 
+}
 
   findAll() {
     return `This action returns all auth`;
